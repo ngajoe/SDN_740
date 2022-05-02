67c741024858c704d35f44116c2bdf53265fd8ed
ngajoe <ngajoe@gmail.com>
2022-05-01 20:45:40 -0500

    Add files via upload

--- intel_script.py
+++ intel_script.py
@@ -10,22 +10,28 @@ import time
 from collections import Counter
 import http.client
 import socket
+import requests
+import struct
 
 #GLOBALS
 blockedIPs = {} # SrcIP and Timestamp
 packetTrack = []
 expireTrack = 60 #How long before removing packets in tracking (seconds)
 
+
 #RULE VARIABLES
 MAX_DstIP   = 2
 MAX_DstPort = 3
-MAX_Flows   = 10
+MAX_Flows   = 20
 MAX_TimeLive= 61 #TimeLive should be longer than expireTrack
+whitelist = ['128.105.146.150', '108.69.65.69', '155.98.38.240']
 
 expireBlock = 30 #How long before removing blocked IPs
 fwController = sys.argv[1]
 #fwController = "155.98.38.240" #Hardcoded - Switch out
-mbPort = 1500
+mbPort = 8598
+sock = socket.socket()
+sock.bind(('', mbPort))
 
 #PACKET CLASS
 class Packet:
@@ -72,19 +78,67 @@ def readfile(filename):
     f.close()
     return packetlist
 
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
 #READ IN SOCKET
-def listenport(port):
+def listenport():
     packetlist = []
-    sock = socket.socket()
-    sock.bind(('', port))
+    sock.settimeout(15)
     sock.listen(1)
-    Print("Listening...")
-    c, addr = sock.accept()
-    j = c.recv(8192)
-    c.close()
+    print("Listening...")
+
     try:
-        data = jsonData(j)
+        c, addr = sock.accept()
+        j = recv_msg(c)
+        c.close()
+    except socket.timeout :
+        print("Timout occured...")
+        #sock.close()
+        return packetlist
+
+    data = json.loads(j.decode("utf-8"))
+    for i in data: #For each packet in json
+            proto = data[i]["Protocol"]
+            #print(proto)
+            ts = data[i]["Timestamp"]
+            #print(ts)
+            SrcIP = data[i]["SrcIP"]
+            #print(SrcIP)
+            SrcPort = data[i]["SrcPort"]
+            #print(SrcPort)
+            DstIP = data[i]["DstIP"]
+            #print(DstIP)
+            DstPort = data[i]["DstPort"]
+            #print(DstPort)
+            packetlist.append(Packet(proto, ts, SrcIP, SrcPort, DstIP, DstPort))
+    '''
+    try:
+        data = jsonData(j.decode("utf-8"))
         for i in data: #For each packet in json
+            print(i)
             proto = data[i]["Protocol"]
             ts = data[i]["Timestamp"]
             SrcIP = data[i]["SrcIP"]
@@ -94,6 +148,7 @@ def listenport(port):
             packetlist.append(Packet(proto, ts, SrcIP, SrcPort, DstIP, DstPort))
     except:
         packetlist = []
+    '''
     return packetlist
 
 
@@ -101,21 +156,31 @@ def listenport(port):
 print("Ready for Action...")
 newpackets = []
 
+
 while True:
-    newpackets = listenport(mbPort)
+    newpackets = listenport()
     if (len(newpackets) == 0):
-        continue
+        print("No Packets Received")
 
     print("-START-")
     timepoint = time.time()
+    
 
-    #packetTrack.append(newpackets[0]) #For trial!
+
+    #packetTrack.append(newpackets[0])
     print("Time: " + str(timepoint))
     print("Packets Received: " + str(len(newpackets)))
 
-    #Remove any new packets that are blocked
-    for badIP in blockedIPs:
-        f_packets = list(filter(lambda pkt: pkt.srcIP != badIP, newpackets))
+    #Whitelisted IPs
+    for goodIP in whitelist:
+        newpackets = list(filter(lambda pkt: pkt.srcIP != goodIP, newpackets))
+    print("After whitelist filter: "+ str(len(newpackets)))
+
+    f_packets = newpackets
+
+    #Remove any new packets that are already blocked
+    for badIP, ts in blockedIPs.items():
+        f_packets = list(filter(lambda pkt: pkt.srcIP != badIP, f_packets))
     print("After blocked filter: " + str(len(f_packets)))
 
     #Remove any new packets that are identical to those in the list
@@ -154,28 +219,34 @@ while True:
 
     # Create Blocks by Rules
 
-    for s in evalDict:
+    for s, t in evalDict.items():
         # RULE 1: Too Many DST IPs per SRC IP
         if (len(evalDict[s]) > MAX_DstIP):
             newBlocks.append(s)
-            print("Exceeded MAX_DstIP: " + s)
+            print("Exceeded MAX_DstIP: " + str(s))
         for d in evalDict[s]:
-            # RULE 2: Too Many DST PORTS per SRC IP
+        # RULE 2: Too Many DST PORTS per SRC IP
             if (len(evalDict[s][d]) > MAX_DstPort):
                 newBlocks.append(s)
-                print("Exceeded MAX_DstPort: " + s)
+                print("Exceeded MAX_DstPort: " + str(s))
 
-    # RULE 3: Too many Flows
+    # RULE 3: Too many Flows (Packets)
     for s, count in CountSourceIPs.items():
         if (count > MAX_Flows):
             newBlocks.append(s)
-            print("Exceeded MAX_Flows: " + s)
+            print("Exceeded MAX_Flows: " + str(s))
+
+    # RULE 3A: Flagged as SYNFLOOD
+    for p in f_packets:
+        if ((p.protocol == "synflood") and (s.srcIP not in newBlocks))
+            newBlocks.append(s.srcIP)
+            print("SYNflood Match: " + str(s))
 
     # RULE 4: Flow too long-lived
-    for s in newpackets:
-        if (s.timestamp > timepoint-MAX_TimeLive):
-            newBlocks.append(s)
-            print("Exceeded MAX_TimeLive: " + s)
+    for s in f_packets:
+        if (s.timestamp < timepoint-MAX_TimeLive):
+            newBlocks.append(s.srcIP)
+            print("Exceeded MAX_TimeLive: " + str(s.srcIP))
 
     # Uniquify Blocks
     newBlocks = list(set(newBlocks))
@@ -185,6 +256,20 @@ while True:
 
     #Issue new block commands
     for ip in newBlocks:
+
+        #try:
+        url = "http://" + fwController + ":8080/wm/ipblacklist/ipblacklist/json"
+        payload = "{\n  \"ip\": \"" + ip +"\",\n}"
+        headers = { 'Content-Type': 'text/plain' }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text)
+        #except:
+        #print("!!HTTP sending error!!")
+        #print(url)
+        #print(headers)
+        #print(payload)
+
+        '''
         conn = http.client.HTTPSConnection(fwController, 8080)
         payload = "{\n  \"ip\": \"" + ip +"\",\n}"
         headers = {
@@ -194,15 +279,32 @@ while True:
         res = conn.getresponse()
         data = res.read()
         print(data.decode("utf-8"))
+        '''
         
     #Update Blocked List
-    for ip in newBlocks:
-        blockedIPs.update({ip, timepoint})
+    for badIP in newBlocks:
+        blockedIPs[badIP] = timepoint
+        packetTrack = list(filter(lambda pkt: pkt.srcIP != badIP, packetTrack))
+
 
     #Time-based cleanup
     #Unblock IPs longer than expire time
     for ip, ts in blockedIPs.items():
-        if (ts + expireBlock > timepoint):
+        if (ts + expireBlock < timepoint):
+
+            #try:
+            url = "http://" + fwController + ":8080/wm/ipblacklist/ipblacklist/json"
+            payload = "{\n  \"ip\": \"" + ip +"\",\n}"
+            headers = { 'Content-Type': 'text/plain' }
+            response = requests.request("DELETE", url, headers=headers, data=payload)
+            print(response.text)
+            #except:
+            #    print("!!HTTP sending error!!")
+            #    print(url)
+            #    print(headers)
+            #    print(payload)
+
+            '''
             conn = http.client.HTTPSConnection(fwController, 8080)
             payload = "{\n  \"ip\": \"" + ip +"\",\n}"
             headers = {
@@ -212,13 +314,15 @@ while True:
             res = conn.getresponse()
             data = res.read()
             print(data.decode("utf-8"))
+            '''
+
     #Remove unblocked IPs from list
     blockedIPs = {ip:ts for ip, ts in blockedIPs.items() if (ts + expireBlock > timepoint)}
     print("Blocked IPs:")
     print(blockedIPs)
 
     #Remove any packets that are older than expireTrack seconds
-    for packet in packetTrack:
-        packetTrack = list(filter(lambda pkt: pkt.timestamp > timepoint-expireTrack , packetTrack))
+    #for packet in packetTrack:
+    packetTrack = list(filter(lambda pkt: pkt.timestamp > timepoint-expireTrack , packetTrack))
     print("Flows Tracked: " + str(len(packetTrack)))
     print("--END--")
